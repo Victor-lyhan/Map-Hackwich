4a0eb79421df0f8977c89c5c218e8c9c75c13433
Victor Han <liyanghan1112@gmail.com>
2022-07-27 09:44:41 -0500

    Initial Commit

---
+++ Map Hackwich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Map Hackwich
+//
+//  Created by Victor Han on 2022/7/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Map Hackwich/Map_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwichApp.swift
+//  Map Hackwich
+//
+//  Created by Victor Han on 2022/7/27.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
